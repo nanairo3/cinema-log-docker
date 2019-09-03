8860e0d44147493e06c973ee46ea3ef09d5c0050
EC2 Default User <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-03 02:31:56 +0000

    UserModel作成,

--- app/controllers/cinema_pages_controller.rb
+++ app/controllers/cinema_pages_controller.rb
@@ -2,13 +2,4 @@ class CinemaPagesController < ApplicationController
   def home
     @cinemas = Cinema.all
   end
-
-  def show
-  end
-
-  def create
-  end
-
-  def new
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_004342) do
+ActiveRecord::Schema.define(version: 2019_09_03_022730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2019_09_03_004342) do
     t.text "content"
     t.integer "user_id"
     t.integer "cinema_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
