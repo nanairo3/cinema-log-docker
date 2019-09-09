79652fd8ec5433f8a5951228d1dbbd614ee9443d
nanairo3 <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-09 11:55:48 +0000

    Login module error

---
+++ spec/system/user_logout_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'ログアウト機能', type: :system do
+    include LoginSupport
+    before do
+        sign_in_as FactoryBot.create(:user)
+    end
+    
+    it 'ログアウトする' do
+        click_link 'Logout'
+        expect(page).to have_content 'ログアウトしました。'
+    end
+end
