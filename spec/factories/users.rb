d07875407c50b22b96bc9e323f370b52f1da40d7
nanairo3 <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-09 06:40:34 +0000

    user model test add

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    name {'userA'}
+    email{'a@example.com'}
+    password{'apassword'}
+    password_confirmation{'apassword'}
+  end
+  
+  factory :other_user, class: 'User' do
+    sequence(:name) { |n| "アザーユーザー_#{n}" }
+    sequence(:email) { |n| "other_#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'name, email,passwordのとき成功' do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+  
+  it 'emailがnilのとき無効' do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+  
+  it 'nameがnilのとき無効' do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+  end
+  
+  it 'passwordがnilのとき無効' do
+     user = FactoryBot.build(:user, password: nil)
+     user.valid?
+     expect(user.errors[:password]).to include("can't be blank")
+  end
+  
+  it 'passwordが6文字以下で無効' do
+      user = FactoryBot.build(:user, password: 'test')
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+  end
+  
+  it 'emailがすでにあるとき無効' do
+      FactoryBot.create(:user, email: 'test@example.com')
+      user = FactoryBot.build(:user, email: 'test@example.com')
+      user.valid?
+      expect(user.errors[:email]).to include("has already been taken")
+  end
+  
+
 end
