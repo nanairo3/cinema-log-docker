584b3817da562b7850f1553480ccf2c9049c8106
nanairo3 <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-09 04:43:43 +0000

    post,create,rollback-fix(optional: true)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 140}}
     validates :cinema_id, {presence: true}
-    belongs_to :user
-    belongs_to :cinema
+    belongs_to :user, optional: true
+    belongs_to :cinema, optional: true
     
     def user
-        #return User.find(self.user_id)
+        return User.find(self.user_id)
     end
 end
