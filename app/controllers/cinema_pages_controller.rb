816c2d04053eec8e3958835315b9f50c935a7023
nanairo3 <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-12 09:03:36 +0000

    layout fix

--- app/controllers/cinema_pages_controller.rb
+++ app/controllers/cinema_pages_controller.rb
@@ -14,4 +14,48 @@ class CinemaPagesController < ApplicationController
       end
     end
   end
+  
+  def show
+    @movie = Movie.details(params[:id])
+    if @movie["credits"]["cast"].count > 6
+      @cast_range = 6
+    else
+      @cast_range = @movie["credits"]["cast"].count
+    end
+    @trailers = Movie.trailers(params[:id])
+
+    @directors = {}
+    @writers = {}
+    @story = {}
+    @screenplay = {}
+    @novel = {}
+    @music = {}
+    @movie["credits"]["crew"].each do |crew|
+      if crew["job"]=="Director" then @directors[crew["name"]] = crew["id"] end
+      if crew["job"]=="Writer" then @writers[crew["name"]] = crew["id"] end
+      if crew["job"]=="Story" then @story[crew["name"]] = crew["id"] end
+      if crew["job"]=="Screenplay" then @screenplay[crew["name"]] = crew["id"] end
+      if crew["job"]=="Novel" then @novel[crew["name"]] = crew["id"] end
+      if crew["job"]=="Original Music Composer" then @music[crew["name"]] = crew["id"] end
+    end
+    @directors_count = @directors.size
+    @writers_count = @writers.size
+    @story_count = @story.size
+    @screenplay_count = @screenplay.size
+    @novel_count = @novel.size
+    @music_count = @music.size
+
+
+    # get current user's comment
+    @current_user_comment = Comment.find_by(user_id: current_user.id, movie_id: @movie["id"]) if logged_in?
+    # create new comment
+    @comment = Comment.new if logged_in? && watched_check(@movie["id"])
+    if Movie.exists?(@movie["id"])
+      # find movie object from database
+      @movie_db = Movie.find(@movie["id"])
+      # paginate all of the movie's comments
+      @comments = @movie_db.comments.paginate(page: params[:page])
+    end
+  end
+  
 end
