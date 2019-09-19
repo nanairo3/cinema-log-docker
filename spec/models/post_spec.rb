81d4e587977a95d4141b2a8fc0254519472f1513
nanairo3 <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-19 07:07:27 +0000

    model test fix

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it 'is valid with a content' do
-    post = FactoryBot.build(:post)
+    post = build(:post)
     expect(post).to be_valid
   end
 
   it 'is invalid without an content' do
-    post = FactoryBot.build(:post, content: nil)
+    post = build(:post, content: nil)
     post.valid?
     expect(post.errors[:content]).to include('を入力してください')
   end
