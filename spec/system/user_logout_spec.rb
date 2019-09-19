e76b2b6c4003e1b790b1dee29d52519f79a4ed85
nanairo3 <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-19 07:18:22 +0000

    system test fix

--- spec/system/user_logout_spec.rb
+++ spec/system/user_logout_spec.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 describe 'ログアウト機能', type: :system do
   before do
-    sign_in_as FactoryBot.create(:user)
+    sign_in_as create(:user)
   end
-
+  
   it 'ログアウトする' do
     click_link 'Logout'
     expect(page).to have_content 'ログアウトしました。'
