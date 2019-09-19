459ccae95e0325aacb1d9190c08eaabab201ad73
nanairo3 <ec2-user@ip-172-31-43-190.ap-northeast-1.compute.internal>
2019-09-19 07:00:20 +0000

    FactoryBot fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,24 +1,8 @@
-# frozen_string_literal: true
-
 FactoryBot.define do
   factory :user do
-    name { 'userA' }
+    sequence(:name) { |n| "user#{n}" }
     sequence(:email) { |n| "a#{n}@example.com" }
-    password { 'apassword' }
-    password_confirmation { 'apassword' }
-  end
-
-  factory :other_user, class: 'User' do
-    sequence(:name) { |n| "otherユーザー_#{n}" }
-    sequence(:email) { |n| "other_#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
-  end
-
-  factory :test_user, class: 'User' do
-    name { 'test_user' }
-    email { 'test@example.com' }
-    password { 'testpass' }
-    password_confirmation { 'testpass' }
   end
 end
