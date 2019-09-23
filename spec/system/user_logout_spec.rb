afd6cc724ff42d274f625311d3eeaea12f400283
nanairo3 <nao23333@gmail.com>
2019-09-23 20:34:49 +0900

    edit

--- spec/system/user_logout_spec.rb
+++ spec/system/user_logout_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 describe 'ログアウト機能', type: :system do
+  let!(:user) { create(:user) }
+
   before do
-    sign_in_as create(:user)
+    sign_in_as user
   end
-  
+
   it 'ログアウトする' do
     click_link 'Logout'
     expect(page).to have_content 'ログアウトしました。'
